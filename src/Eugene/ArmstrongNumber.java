d3dbb3b562bf9e3af0ccac0c91afbce8a51961d0
Yevhen Marushchak <maruschak1992@gmail.com>
2022-10-19 12:57:07 -0400

    Code written

--- src/Eugene/ArmstrongNumber.java
+++ src/Eugene/ArmstrongNumber.java
@@ -1,4 +1,18 @@
 package Eugene;
 
 public class ArmstrongNumber {
+    public static boolean isArmstrong (int num) {
+
+        String numStr = "" + num;
+        int len = numStr.length();
+        String[] arr = numStr.split("");
+
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            int multiple_3 = 0;
+            multiple_3 += Math.pow(Integer.parseInt(arr[i]), len);
+            sum += multiple_3;
+        }
+        return num == sum;
+    }
 }
--- src/Eugene/ReverseNumber.java
+++ src/Eugene/ReverseNumber.java
@@ -1,4 +1,18 @@
 package Eugene;
 
 public class ReverseNumber {
+    public static int reverseNegativeNumber (int num) {
+
+        num = -1 * num;
+        String numStr = "" + num;
+        String[] numArr = numStr.split("");
+        String result = "";
+
+        for (int i = numArr.length - 1; i >= 0; i--) {
+            result += numArr[i];
+        }
+        int reversedNumber = Integer.parseInt(result);
+
+        return reversedNumber * (-1);
+    }
 }
